699e8e8b2eb8fe21772ea785a84a15735b85e34c
unknown <camirmunozc@gmail.com>
2023-08-21 21:15:17 -0300

    cambios posteriores

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ let app = express();
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT,() => {
     console.log('servidor corriendo en el puerto ${PORT}');
